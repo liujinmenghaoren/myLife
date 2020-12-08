63fc0cf4955352ea2c401735256cce4582c7a45b
liujinmeng <1325479537@qq.com>
2020-12-09 00:55:58 +0800

    create my resume

---
+++ LJM-Resume/resume.js
@@ -0,0 +1,18 @@
+function change(){
+	var imgArr = [
+		"url(../images/background/bg2.jpg)",
+		"url(../images/background/bg14.gif)",
+		"url(../images/background/bg6.gif)",
+		"url(../images/background/bg15.gif)",
+		"url(../images/background/bg5.gif)",
+		"url(../images/background/bg11.jpg)",
+		"url(../images/background/bg7.jpg)",
+		"url(../images/background/bg1.jpg)",
+		"url(../images/background/bg22.jpg)",
+		"aa"
+	];
+	var index = parseInt(Math.random()*(imgArr.length-1));
+	var currentImage = imgArr[index];
+	//alert(currentImage);
+	document.getElementById("body").style.backgroundImage = currentImage;
+}
